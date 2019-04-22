3055bd5959d72bf8fb0d26f8424ec3673ae0fc93
Keith Burgie <krburgie@gmail.com>
2019-04-22 03:11:05 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -32,7 +32,19 @@ function onKeyDownHandler(e) {
 onKeyDownHandler();
 
 document.addEventListener('keydown', function(e) {
+  const k = e.key;
   
+  if (key === alphabet[index]) {
+    index++;
+    
+    if (index === alphabet.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 });
 
 function init() {
